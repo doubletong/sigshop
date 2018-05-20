7629cb42743c8ed073df17dd653aa48fea414b60
doubletong <service@heiniaozhi.cn>
2018-05-20 17:16:06 +0800

    up

---
+++ SIG.Data/Entity/Goods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SIG.Data.Entity
+{
+    public partial class Goods:IAuditedEntity
+    {
+        //public Goods()
+        //{
+        //    this.EntityName = EntityNames.Goods;
+        //    //this.GoodsSpecialPropertyValues = new HashSet<GoodsSpecialPropertyValue>();
+        //    this.GoodsPhotos = new HashSet<GoodsPhoto>();
+        //}
+        public int Id { get; set; }
+        [ForeignKey("GoodsCategory")]
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string FullDescription { get; set; }
+        public string Thumbnail { get; set; }
+        public int ViewCount { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+
+        public virtual GoodsCategory GoodsCategory { get; set; }
+        //public virtual ICollection<GoodsSpecialPropertyValue> GoodsSpecialPropertyValues { get; set; }  
+       
+        //public virtual ICollection<GoodsPhoto> GoodsPhotos { get; set; }
+      
+        //public ICollection<OrderDetail> OrderDetails { get; set; }
+       
+        //public ICollection<Cart> Carts { get; set; }
+    }
+}
---
+++ SIG.Data/Entity/GoodsCategory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIG.Data.Entity
+{
+    public partial class GoodsCategory : IAuditedEntity
+    {
+        public GoodsCategory()
+        {
+         
+            //this.GoodAttributes = new HashSet<GoodsAttribute>();
+            this.Goodses = new HashSet<Goods>();
+            //this.GoodsSpecialPropertys = new HashSet<GoodsSpecialProperty>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string SeoName { get; set; }
+        public string Thumbnail { get; set; }
+        public int Importance { get; set; }
+        public bool Active { get; set; }
+        public bool Recommend { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+
+        //public virtual ICollection<GoodsAttribute> GoodAttributes { get; set; }
+        public virtual ICollection<Goods> Goodses { get; set; }
+        //public virtual ICollection<GoodsSpecialProperty> GoodsSpecialPropertys { get; set; }
+    }
+}
