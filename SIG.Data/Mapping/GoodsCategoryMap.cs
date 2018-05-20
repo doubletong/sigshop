05e34afb455519f1417af6a324858c7d778f7733
doubletong <service@heiniaozhi.cn>
2018-05-20 17:16:53 +0800

    oo

---
+++ SIG.Data/Mapping/GoodsCategoryMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SIG.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIG.Data.Mapping
+{
+    public class GoodsCategoryMap : IEntityTypeConfiguration<GoodsCategory>
+    {
+        public void Configure(EntityTypeBuilder<GoodsCategory> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.ToTable("GoodsCategorySet");
+            builder.Property(p => p.Title).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Importance).IsRequired();
+            builder.Property(p => p.Active).IsRequired();
+            builder.Property(p => p.CreatedDate).IsRequired().HasColumnType("datetime");
+            builder.Property(p => p.CreatedBy).HasMaxLength(50);
+            builder.Property(p => p.UpdatedBy).HasMaxLength(50);
+        }
+    }
+}
---
+++ SIG.Data/Mapping/GoodsMap.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SIG.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIG.Data.Mapping
+{
+    public class GoodsMap : IEntityTypeConfiguration<Goods>
+    {
+        public void Configure(EntityTypeBuilder<Goods> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.ToTable("GoodsSet");
+
+            builder.Property(p => p.Active).IsRequired();
+            builder.Property(p => p.CreatedDate).IsRequired().HasColumnType("datetime");
+            builder.Property(p => p.CreatedBy).HasMaxLength(50);
+            builder.Property(p => p.UpdatedBy).HasMaxLength(50);
+
+            builder.HasOne(p => p.GoodsCategory)
+              .WithMany(h => h.Goodses)
+              .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
